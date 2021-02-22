224a954b5cb300103971142516af9619e81e6115
med100s <61676013+med100s@users.noreply.github.com>
2021-02-22 14:13:30 +0500

    Update nikita's notes.py

--- nikita's notes.py
+++ nikita's notes.py
@@ -1,11 +1,1 @@
-generated[0] = str('привет')
-for i in range(15):
-    generated = generate(model, tok, generated[0], num_beams=10)
-    fulltext = ' '.join(generated[0].split())
-    generated[0] = ' '.join(generated[0].split()[-15:])
-    print('1',fulltext)
-
-print('2',generated[0])
-
-
-print(fulltext)
+https://colab.research.google.com/github/sberbank-ai/ru-gpts/blob/master/examples/ruGPT3XL_generation.ipynb#scrollTo=8L-CF6uLjpPZ

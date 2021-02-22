d27a137e4fcceb1dbf43f37a07ae64169aa22487
med100s <61676013+med100s@users.noreply.github.com>
2021-02-22 14:13:53 +0500

    Update nikita's notes.py

--- nikita's notes.py
+++ nikita's notes.py
@@ -1,1 +1,3 @@
 https://colab.research.google.com/github/sberbank-ai/ru-gpts/blob/master/examples/ruGPT3XL_generation.ipynb#scrollTo=8L-CF6uLjpPZ
+  
+https://github.com/sberbank-ai/ru-gpts/blob/master/examples/ruGPT3XL_generation.ipynb

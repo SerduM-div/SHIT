679ee94397831f82d4204e136dc81c264b0d0fb4
med100s <61676013+med100s@users.noreply.github.com>
2021-02-22 11:17:02 +0500

    Update nikita's notes.py
    почти финальная версия котороая делает то что нужно но не отображает корректно

--- nikita's notes.py
+++ nikita's notes.py
@@ -1,23 +1,11 @@
-prokladka = str('привет')
-for i in range(5):
-    generated = generate(model, tok, prokladka, num_beams=10)
-    prokladka = ' '.join(generated[0].split()[:7])
-    print(generated[0])
-
-    
-    prokladka = str('привет')
-for i in range(5):
-    generated = generate(model, tok, prokladka, num_beams=10)
-    prokladka = ' '.join(generated[0].split()[:15])
-    print('1',prokladka)
-    fulltext += ' '.join(generated[0].split())
-    print('2',fulltext)
-print('3',fulltext)
-
-
-
 generated[0] = str('привет')
-for i in range(5):
+for i in range(15):
     generated = generate(model, tok, generated[0], num_beams=10)
-    generated[0] = ' '.join(generated[0].split()[:7])
-    print(generated[0])
+    fulltext = ' '.join(generated[0].split())
+    generated[0] = ' '.join(generated[0].split()[-15:])
+    print('1',fulltext)
+
+print('2',generated[0])
+
+
+print(fulltext)

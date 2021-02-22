b104d033f8b9ed198630f9be727722873cc2cad1
med100s <61676013+med100s@users.noreply.github.com>
2021-02-22 10:52:21 +0500

    Update nikita's notes.py

--- nikita's notes.py
+++ nikita's notes.py
@@ -3,3 +3,21 @@ for i in range(5):
     generated = generate(model, tok, prokladka, num_beams=10)
     prokladka = ' '.join(generated[0].split()[:7])
     print(generated[0])
+
+    
+    prokladka = str('привет')
+for i in range(5):
+    generated = generate(model, tok, prokladka, num_beams=10)
+    prokladka = ' '.join(generated[0].split()[:15])
+    print('1',prokladka)
+    fulltext += ' '.join(generated[0].split())
+    print('2',fulltext)
+print('3',fulltext)
+
+
+
+generated[0] = str('привет')
+for i in range(5):
+    generated = generate(model, tok, generated[0], num_beams=10)
+    generated[0] = ' '.join(generated[0].split()[:7])
+    print(generated[0])
